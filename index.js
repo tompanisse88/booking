5b2ae66b7b27be09ace16c8b14fdf6b29b761fe2
Tom Nilsson <tomnilsson@Toms-MacBook-Pro.local>
2021-11-20 12:56:58 +0100

    first heroku push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from 'body-parser';
 import cors from "cors";
+import path from "path";
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -10,23 +11,31 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+//use routes
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 const dbURI = 'mongodb+srv://tompanisse:tompanisse@deskbookingdb.lnn2w.mongodb.net/DeskDB?retryWrites=true&w=majority';
 
 mongoose.connect(process.env.MONGODB_URI || dbURI, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+
+//if in production
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'))
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+
+  mongoose.connection.once("open", function(){
+    app.listen(PORT, () => console.log("listening on port: " + PORT));
+  }).on("error", function(error){
+    console.log("ERROR IS:", error);
+  });
 }
 
-mongoose.connection.once("open", function(){
-  app.listen(PORT, () => console.log("listening on port: " + PORT));
-}).on("error", function(error){
-  console.log("ERROR IS:", error);
-});
