08e4523da404411cccc8abaab7e3f35b98bc2cef
Tompanisse88 <tom.nilsson88@gmail.com>
2021-11-20 14:37:46 +0100

    changed API axios baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
  
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://deskbookingapp.herokuapp.com'});
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
