43911bf8a57ccac06d41c237c17b6acd6e3f98ee
Tom Nilsson <tomnilsson@Toms-MacBook-Pro.local>
2021-11-20 13:19:19 +0100

    tredje

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-import User from '../models/users.js';
+import User from './models/users.js';
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
